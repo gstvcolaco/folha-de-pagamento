8399c4f7ba73fe9cf97dd29cd5ad78ff5755b205
gstvcolaco <gustavocolaco2014@gmail.com>
2025-04-08 16:32:02 -0300

    controller inicial

---
+++ FolhaDePagamento/src/main/java/com/gstvcolaco/FolhaDePagamento/controller/PagamentoController.java
@@ -0,0 +1,10 @@
+package com.gstvcolaco.FolhaDePagamento.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/folha-de-pagamento"})
+public class PagamentoController {
+
+}
