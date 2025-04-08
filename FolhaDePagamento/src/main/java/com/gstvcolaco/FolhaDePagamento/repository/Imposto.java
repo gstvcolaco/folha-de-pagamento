8c134b7ee1435ab5512f3863499a9bad95c3e6d7
gstvcolaco <gustavocolaco2014@gmail.com>
2025-04-08 14:39:50 -0300

    Repository

---
+++ FolhaDePagamento/src/main/java/com/gstvcolaco/FolhaDePagamento/repository/Imposto.java
@@ -0,0 +1,6 @@
+package com.gstvcolaco.FolhaDePagamento.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Imposto extends JpaRepository<Imposto, Integer> {
+}
